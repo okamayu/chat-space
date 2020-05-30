0ea99fe91ee86c403d6691327cf2c48c29ba6a2d
okamayu <mayu.okagawa0511@gmail.com>
2020-05-30 20:24:22 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,17 +2,17 @@ $(function(){
       function buildHTML(message){
       if ( message.image ) {
         var html =
-          `<div class="chat-main">
-            <div class="chat-main_message-list_message-info">
-              <div class="chat-main_message-list_message-info_talker">
+          `<div class="messageBox" data-message-id=${message.id}>
+            <div class="chat-main__message-list__message-info">
+              <div class="chat-main__message-list__message-info__talker">
                 ${message.user_name}
               </div>
-              <div class="chat-main_message-list_message-info_date">
+              <div class="chat-main__message-list__message-info__date">
                 ${message.created_at}
               </div>
             </div>
-            <div class="chat-main_message-list_message-text">
-              <p class="lower-message_content">
+            <div class="chat-main__message-list__message-text">
+              <p class="lower-message__content">
                 ${message.content}
               </p>
             </div>
@@ -21,17 +21,17 @@ $(function(){
         return html;
       } else {
         var html =
-          `<div class="chat-main">
-            <div class="chat-main_message-list_message-info">
-              <div class="chat-main_message-list_message-info_talker">
+          `<div class="messageBox" data-message-id=${message.id}>
+            <div class="chat-main__message-list__message-info">
+              <div class="chat-main__message-list__message-info__talker">
                 ${message.user_name}
               </div>
-              <div class="chat-main_message-list_message-info_date">
+              <div class="chat-main__message-list__message-info__date">
                 ${message.created_at}
               </div>
            </div>
-           <div class="chat-main_message-list_message-text">
-             <p class="lower-message_content">
+           <div class="chat-main__message-list__message-text">
+             <p class="lower-message__content">
                ${message.content}
              </p>
            </div>
@@ -39,6 +39,36 @@ $(function(){
        return html;
      };
    }
+   var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.messageBox:last').data("message-id");
+    
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
 $('#new_message').on('submit', function(e){
    e.preventDefault();
    var formData = new FormData(this);
@@ -62,4 +92,7 @@ $('#new_message').on('submit', function(e){
       alert("メッセージ送信に失敗しました");
   });
 })
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
